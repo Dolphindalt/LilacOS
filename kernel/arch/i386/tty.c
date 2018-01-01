be7fcc464405719e827c587bf99a8305fd4a0bf4
Dalton <dolphindalt@gmail.com>
2018-01-01 13:10:42 -0800

    ssp protection and more terminal features.

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -4,6 +4,8 @@
 #include <string.h>
 
 #include <kernel/tty.h>
+#include <kernel/system.h>
+#include <kernel/ssp.h>
 
 #include "vga.h"
 
@@ -33,9 +35,54 @@ void terminal_setcolor(uint8_t color) {
 	terminal_color = color;
 }
 
+void terminal_updatecursor()
+{
+    unsigned temp;
+    temp = terminal_column + VGA_WIDTH * terminal_row;
+    outb(0x3D4, 14);
+    outb(0x3D5, temp >> 8);
+    outb(0x3D4, 15);
+    outb(0x3D5, temp);
+}
+
+void terminal_clearline(uint8_t from, uint8_t to)
+{
+    uint16_t i = VGA_WIDTH * from;
+    for(; i < (VGA_WIDTH * (to+1)); i++)
+        terminal_buffer[i] = 0x0;
+}
+
+void terminal_scrollup(uint8_t line)
+{
+    uint16_t i;
+    for(i = 0; i < (VGA_WIDTH * VGA_HEIGHT)-1; i++)
+        terminal_buffer[i] = terminal_buffer[i + VGA_WIDTH * line];
+    terminal_clearline(VGA_HEIGHT-1-line, VGA_HEIGHT-1);
+    if((((signed)terminal_column) - line) < 0)
+    {
+        terminal_column = 0;
+        terminal_row = 0;
+    }
+    else terminal_column -= line;
+}
+
+void terminal_checknewline()
+{
+    if(terminal_row >= VGA_HEIGHT-1) terminal_scrollup(1);
+}
+
 void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
-	const size_t index = y * VGA_WIDTH + x;
-	terminal_buffer[index] = vga_entry(c, color);
+	switch(c)
+    {
+        case '\n': terminal_row++; terminal_column = -1; break;
+        default: 
+        {
+            const size_t index = y * VGA_WIDTH + x;
+            terminal_buffer[index] = vga_entry(c, color); 
+            break;
+        }
+    }
+    terminal_checknewline();
 }
 
 void terminal_putchar(char c) {
@@ -46,6 +93,7 @@ void terminal_putchar(char c) {
 		if (++terminal_row == VGA_HEIGHT)
 			terminal_row = 0;
 	}
+	terminal_updatecursor();
 }
 
 void terminal_write(const char* data, size_t size) {
---
+++ kernel/include/kernel/system.h
@@ -0,0 +1,23 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include<stdint.h>
+
+static inline void outb(uint16_t port, uint8_t val)
+{
+    asm volatile("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+static inline void io_wait()
+{
+    asm volatile("jmp 1f\n\t" "1:jmp 2f\n\t" "2:");
+}
+
+#endif
